851536d9ba9344cb495fcb386b4e27165e76662a
Zheng Li <zheng.li@morganstanley.com>
2022-05-20 00:32:52 -0400

    .

---
+++ win/myvscodeext-in-use.cmd
@@ -0,0 +1,60 @@
+rem extension details at
+rem
+rem https://marketplace.visualstudio.com/items?itemName=fabiospampinato.vscode-todo-plus
+
+for %%e in (
+
+	ms-vscode.vs-keybindings/0.2.0
+	vscode-icons-team.vscode-icons/11.8.0
+	vscodevim.vim/1.21.10
+
+	oderwat.indent-rainbow/7.5.0
+	shardulm94.trailing-spaces/0.3.1
+
+	christian-kohler.path-intellisense/2.4.0
+
+	ryanluker.vscode-coverage-gutters/2.6.0
+	mechatroner.rainbow-csv/1.7.0
+	IBM.output-colorizer/0.1.2
+
+	rem markdown
+	yzhang.markdown-all-in-one/3.4.0
+	shd101wyy.markdown-preview-enhanced/0.5.22
+	bierner.markdown-preview-github-styles/0.1.6
+	yzane.markdown-pdf/1.4.4
+
+	rem git
+	ryu1kn.annotator/0.11.0
+	mhutchie.git-graph/1.30.0
+	eamodio.gitlens/11.6.1
+
+	rem workspace
+	EditorConfig.EditorConfig/0.16.4
+
+	rem python
+	ms-python.python/2022.0.1786462952
+	ms-python.vscode-pylance/2022.2.1
+	ms-python.gather/2021.6.0
+	ms-toolsai.jupyter/2022.1.1001775990
+
+	rem golang & rust
+	golang.go/0.26.0
+	rem ms-vscode.go/0.11.7
+	rust-lang.rust/0.6.1
+
+	rem syntax
+	wholroyd.jinja/0.0.8
+	samuelcolvin.jinjahtml/0.10.6
+	rem better jinja
+	zxh404.vscode-proto3/0.2.2
+	redhat.vscode-xml/0.18.1
+	redhat.vscode-yaml/1.2.0
+
+	rem browser
+	ritwickdey.LiveServer/5.6.1
+	auchenberg.vscode-browser-preview/0.5.9
+	msjsdiag.debugger-for-edge/1.0.10
+
+) do (
+	powershell -noprofile -file %~dp0\vscodeInstall.ps1 \\ms\dist\vscodeext\PROJ\%%e
+)
--- win/myvscodeext.cmd
+++ win/myvscodeext.cmd
@@ -4,24 +4,16 @@ rem https://marketplace.visualstudio.com/items?itemName=fabiospampinato.vscode-t
 
 for %%e in (
 
-	ms-vscode.vs-keybindings/0.2.0
-
 	arcticicestudio.nord-visual-studio-code/0.14.0
-	davidhewitt.shebang-language-associator/1.1.1
-
-	vscodevim.vim/1.21.10
-	golang.go/0.26.0
 
 	alefragnani.Bookmarks/10.5.0
 	alefragnani.numbered-bookmarks/6.2.1
 	kdcro101.favorites/2.4.5
 	sleistner.vscode-fileutils/3.4.5
 	patrys.vscode-code-outline/0.0.13
-	ryanluker.vscode-coverage-gutters/2.6.0
 	rem rubymaniac.vscode-paste-and-indent/0.0.8
 	slevesque.vscode-hexdump/1.6.0
 	mitchdenny.ecdc/1.3.0
-	CoenraadS.bracket-pair-colorizer/1.0.61
 
 	annsk.alignment/0.3.0
 	tootone.org-mode/0.5.0
@@ -29,14 +21,8 @@ for %%e in (
 	aaron-bond.better-comments/2.1.0
 	fabiospampinato.vscode-todo-plus/4.17.0
 
-	oderwat.indent-rainbow/7.5.0
-	mechatroner.rainbow-csv/1.7.0
-	IBM.output-colorizer/0.1.2
-
-	shardulm94.trailing-spaces/0.3.1
 	everettjf.filter-line/1.2.10
 	adamwalzer.string-converter/0.1.2
-	christian-kohler.path-intellisense/2.4.0
 	ryu1kn.partial-diff/1.4.0
 	Tyriar.sort-lines/1.7.0
 
@@ -49,9 +35,6 @@ for %%e in (
 	Gruntfuggly.todo-tree/0.0.162
 
 	patbenatar.advanced-new-file/1.2.0
-
-	file-icons.file-icons/1.0.23
-	robertohuertasm.vscode-icons/8.0.0
 
 	berublan.vscode-log-viewer/0.10.3
 	chnicholas.jil-highlighter/0.1.0
@@ -96,17 +79,10 @@ for %%e in (
 	rem testing
 	DavidSchuldenfrei.gtest-adapter/1.8.3-ms1
 
-	rem git
-	ryu1kn.annotator/0.11.0
-	mhutchie.git-graph/1.30.0
-	rem donjayamanne.githistory/0.2.2
-	eamodio.gitlens/11.6.1
-
 	jshinonome.vscode-q/2.2.6
 	ms-vscode.PowerShell/2021.10.1
 
 	rem ms-vscode.csharp/1.23.1
-	rem ms-vscode.go/0.11.7
 
 	rem redhat.java/0.82.0
 	rem vscjava.vscode-java-dependency/0.18.7
@@ -120,30 +96,16 @@ for %%e in (
 	Splunk.splunk/0.2.3
 	kristophbarbour.splunk-linter/0.0.5
 
-	redhat.vscode-xml/0.18.1
-	redhat.vscode-yaml/1.2.0
-
 	rem ms-dotnettools.vscode-dotnet-runtime/0.1.2
 
-	ms-python.python/2022.0.1786462952
-	ms-python.vscode-pylance/2022.2.1
-	ms-python.gather/2021.6.0
-
-	ms-toolsai.jupyter/2022.1.1001775990
 	rem ms-python.anaconda-extension-pack/1.0.0
 	LittleFoxTeam.vscode-python-test-adapter/0.7.0
 
-	msjsdiag.debugger-for-chrome/4.12.12
-	msjsdiag.debugger-for-edge/1.0.10
-
 	GrapeCity.gc-excelviewer/3.0.44
 	tomoki1207.pdf/0.4.3
-	shd101wyy.markdown-preview-enhanced/0.5.22
-	bierner.markdown-preview-github-styles/0.1.6
+
 	rem tht13.rst-vscode/3.0.1
 	cssho.vscode-svgviewer/2.0.0
-
-	yzane.markdown-pdf/1.4.4
 
 	humao.rest-client/0.20.4
 	philosowaffle.openapi-designer/0.2.3
@@ -157,11 +119,7 @@ for %%e in (
 
 	haaaad.ansible/0.2.8
 	vscoss.vscode-ansible/0.5.2
-	wholroyd.jinja/0.0.8
-	samuelcolvin.jinjahtml/0.10.6
-	zxh404.vscode-proto3/0.2.2
 
-	yzhang.markdown-all-in-one/3.4.0
 	rem stardog-union.stardog-rdf-grammars/0.0.6
 
 	mohd-akram.vscode-html-format/0.0.3
@@ -185,8 +143,6 @@ for %%e in (
 	rem Ikuyadeu.r/1.1.8
 
 	JamesBirtles.svelte-vscode/0.9.1
-	octref.vetur/0.14.3
-	sdras.vue-vscode-snippets/1.8.0
 
 	rem vscode-ext.sync-rsync/0.34.7
 	rem Kelvin.vscode-sshfs/1.18.2
@@ -217,11 +173,6 @@ for %%e in (
 
 	hbenl.vscode-test-explorer/2.20.4
 
-	rust-lang.rust/0.6.1
-	
-	ritwickdey.LiveServer/5.6.1
-	auchenberg.vscode-browser-preview/0.5.9
-
 	rem steoates.autoimport/1.5.3
 	rem rbbit.typescript-hero/1.5.0
 	rem esbenp.prettier-vscode/9.0.0
@@ -238,8 +189,6 @@ for %%e in (
 	rem slevesque.perforce/3.1.0
 
 	rem Yinkai15.aemsync/0.2.3
-
-	EditorConfig.EditorConfig/0.16.4
 
 ) do (
 	powershell -noprofile -file %~dp0\vscodeInstall.ps1 \\ms\dist\vscodeext\PROJ\%%e
